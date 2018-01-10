c6ad9d2ca9bc381eda7e0031b27b0c1bd2781981
huangguozhen <huangguozhen@outlook.com>
2018-01-10 10:48:57 +0800

    test account

--- server/config.js
+++ server/config.js
@@ -2,15 +2,15 @@ module.exports = {
   app: {
     server: 'http://ghgylenterprise.intoyun.com',
     version: 'v1',
-    appId: 'b0a5b389c2811e5b0509584942a245df',
-    appSecret: '698c253d37bca988a99870da466c94f1'
+    appId: '5751976dcc9a78a36796bb3af0938005',
+    appSecret: '39484bb40d9c3ebd75fc01226574e375'
   },
   kafka: {
-    topic: 'device-data-b0a5b389c2811e5b0509584942a245df',
-    group: 'b0a5b389c2811e5b0509584942a245df',
+    topic: 'device-data-5751976dcc9a78a36796bb3af0938005',
+    group: '5751976dcc9a78a36796bb3af0938005',
     brokers: 'ghgyldps.intoyun.com:9092',
     sasl_enable: true,
-    appId: 'b0a5b389c2811e5b0509584942a245df',
-    appSecret: '698c253d37bca988a99870da466c94f1'
+    appId: '5751976dcc9a78a36796bb3af0938005',
+    appSecret: '39484bb40d9c3ebd75fc01226574e375'
   }
 }
